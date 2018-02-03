b1f531ad65d3e39c1475aebf33b6adfa0eaa652d
Ryan Birmingham <birm@rbirm.us>
2018-02-02 21:39:14 -0500

    other urlencode works with pubkey for some reason

--- host.sh
+++ host.sh
@@ -7,10 +7,10 @@ openssl rsa -in cert.pem -pubout -out cert.pub
 pubkey=$(cat cert.pub)
 
 # declare mongo host
-curl -X POST "$hub_url/post/variable" --data "{admin_password: $hub_pw, variable = $mongo_host}"
+curl -X POST "$hub_url/post/variable" --data "{admin_password: $hub_pw, variable: $mongo_host}"
 
 # host app
-NODE_ID=$(curl -X POST "$hub_url/post/auth" --data "{admin_password: $hub_pw, pubkey = $pubkey}")
+NODE_ID=$(curl -X POST "$hub_url/post/auth" --data "admin_password=$hub_pw&pubkey= $pubkey")
 
 mongo --host $mongo_host dh_auth --eval 'db.createCollection("users")'
 mongo --host $mongo_host dh_auth --eval 'db.users.createIndex( { "username": 1 }, { unique: true } )'
