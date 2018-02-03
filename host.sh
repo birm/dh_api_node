c36abc8ee0431985296b16d3cf978077a9f2431a
Ryan Birmingham <birm@rbirm.us>
2018-02-02 22:20:50 -0500

    rest before posting

--- host.sh
+++ host.sh
@@ -7,11 +7,11 @@ openssl rsa -in cert.pem -pubout -out cert.pub
 
 
 # host app
-NODE_ID=$(curl -X POST "$hub_url/post/auth" --data "admin_password=$hub_pw&pubkey=$(cat cert.pub)")
+
 
 mongo --host $mongo_host dh_auth --eval 'db.createCollection("users")'
 mongo --host $mongo_host dh_auth --eval 'db.users.createIndex( { "username": 1 }, { unique: true } )'
 
-
-NODE_ID=$(curl -X POST "$hub_url/post/auth" --data "admin_password=HI&pubkey= $pk")
+sleep 5s
+NODE_ID=$(curl -X POST "$hub_url/post/auth" --data "admin_password=$hub_pw&pubkey=$(cat cert.pub)")
 nodejs index.js $NODE_ID $hub_url $mongo_host
